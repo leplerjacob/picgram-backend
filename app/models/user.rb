e15eda04c3e01108a6f4cc8da68347fbb3cb757e
Jacob Lepler <lepler.jacob@gmail.com>
2021-04-30 12:48:15 -0400

    user generate jwt

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,15 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable, 
+
+  #  Including only the user's id into the encrypted token
+  def generate_jwt
+    JWT.encode({
+      id: id, 
+      exp: 60.days.from_now.to_i
+      }, 
+      Rails.application.secrets.secret_key_base
+    )
+  end
 end
