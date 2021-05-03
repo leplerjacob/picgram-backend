15763acaede0f982c78d4229776bef67b88d0070
cperez1012 <perezchristian1012@gmail.com>
2021-05-03 12:41:03 -0400

    added comma after deivse_for

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users,
 
   # To overwrite default rails controller sessions routing. 
   controllers: {
     registrations: :registrations,
     sessions: :sessions
   }
+
 end
