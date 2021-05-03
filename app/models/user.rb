00b0bda7e86255b1a1d6721e87ad81cca394981c
cperez1012 <perezchristian1012@gmail.com>
2021-05-03 12:45:30 -0400

    uncommented devise lines within User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable, 
+  devise :database_authenticatable, :registerable
+        #  :recoverable, :rememberable, :validatable, 
 
   #  Including only the user's id into the encrypted token
   def generate_jwt
