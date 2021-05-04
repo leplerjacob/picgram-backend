abd565d2a1c9c895ce97aff94f4d895a31d055b7
Jacob Lepler <lepler.jacob@gmail.com>
2021-05-04 19:40:31 -0400

    models

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ApplicationRecord
+  has_many :posts
+  has_many :comments
+  has_many :friends, as: :users
+  has_many :followers, as: :users 
+  has_many :following, as: :users
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable
