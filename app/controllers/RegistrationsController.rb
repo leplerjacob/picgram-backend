23838e576f408b81d503d4e5ed7f909bdc9e98b3
Jacob Lepler <lepler.jacob@gmail.com>
2021-05-04 13:11:12 -0400

    added private method to registrationscontroller

--- app/controllers/RegistrationsController.rb
+++ app/controllers/RegistrationsController.rb
@@ -5,7 +5,6 @@ class RegistrationsController < Devise::RegistrationsController
 
   def create
       user = User.new(sign_up_params)
-      # byebug
       if user.save
       token = user.generate_jwt
         render json: token.to_json
@@ -13,4 +12,10 @@ class RegistrationsController < Devise::RegistrationsController
         render json: { errors: { 'email or password' => ['is invalid'] } }, status: :unprocessable_entity
       end
   end
+
+  private
+  def sign_up_params 
+    params.require(:user).permit(:email, :password)
+  end
+
 end
