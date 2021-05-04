5c8a42d0f609abcf32e3ed1c612c8de214a8f226
Jacob Lepler <lepler.jacob@gmail.com>
2021-05-04 19:40:50 -0400

    seeds routes

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,20 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+User.destroy_all
+Post.destroy_all
+Comment.destroy_all
+
+nerly = User.create(email: "nerly@me.com", password: "123asd")
+
+jacob = User.create(email: "jacob@me.com", password: "456fgh")
+
+christian = User.create(email: "christian@me.com", password: "789jkl")
+
+post_one = Post.create(image: "http://somesite.com/123dicde", description: "this is me at the beach. yayyy", user_id: nerly.id)
+
+post_two = Post.create(image: "http://somesite.com/583rnie", description: "this is me at the mall. yayyy", user_id: jacob.id)
+
+post_three = Post.create(image: "http://somesite.com/429f9j", description: "this is me at the dealership. yayyy", user_id: christian.id)
+
+comment_one = Comment.create(text: "this is some text", user_id: nerly.id, post_id: post_two.id)
+comment_two = Comment.create(text: "this is some text", user_id: jacob.id, post_id: post_three.id)
+comment_three = Comment.create(text: "this is some text", user_id: christian.id, post_id: post_one.id)
+
