075bed8a5a388fae99e7bf12beeee12f904ab3f8
cperez1012 <perezchristian1012@gmail.com>
2021-05-13 20:16:07 -0400

    updated models and more

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,16 @@
 class User < ApplicationRecord
   has_many :posts
   has_many :comments
-  # has_many :friendships, dependent: :destroy
-  # has_many :friends, through: :friendships
-  # has_many :followers, as: :users 
-  # has_many :following, as: :users
+  has_many :friendships, dependent: :destroy
+  has_many :friends, through: :friendships
+  has_many :followings
+  has_many :followers, through: :followings, as: :followers
+  # LINE 9: NOT WORKING. See comment below for testing
+  has_many :following, through: :followings, source: :user
+  # **** TEST for LINE 9 *****
+  # initialize var. ex "jacob = User.second"
+  # then call "jacob.following". This should return the users jacob is following
+  # *** TEST ***
 
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
@@ -20,4 +26,5 @@ class User < ApplicationRecord
       Rails.application.secrets.secret_key_base
     )
   end
+
 end
